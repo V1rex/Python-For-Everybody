12f3ac19aa992a473ad799223675619d21c24c3c
Amine Bahij <go4computergeek@gmail.com>
2020-08-12 14:12:10 +0100

    Added the solution for the exercice 2.5

---
+++ 02-variables/additional exerices/Exercice 5.py
@@ -0,0 +1,11 @@
+"""
+Exercise 5:
+        Write a program which prompts the user for a Celsius temperature, convert the temperature to Fahrenheit,
+and print out the converted temperature.
+
+"""
+
+tempC= float(input("Celisius temperature : "))
+tempF = (tempC * 9/5) + 32
+
+print('The temparature in Fahrenheit is :', tempF ,'°F')
