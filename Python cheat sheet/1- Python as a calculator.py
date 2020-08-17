8a9737b3f7620cfbcce1f680292dde66249e0f26
Amine Bahij <go4computergeek@gmail.com>
2020-08-17 22:12:48 +0100

    Added the cheat sheet : 1

---
+++ Python cheat sheet/1- Python as a calculator.py
@@ -0,0 +1,16 @@
+
+
+addition = 2 + 2
+
+subtraction = 50 - 5
+
+multiplication = 5 * 2
+
+division_float = 8 / 5 # this expression returns a float
+
+division_int = 8 // 5 # this expression return an int
+
+remainder_of_division = 8 % 5
+
+power = 5 ** 2 # 5 power 2 return as a result : 25
+
